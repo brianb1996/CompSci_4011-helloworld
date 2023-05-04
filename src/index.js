dba57195001e772bf1860a5ab3a38168c11085dd
brianb1996 <bbredahl1996@gmail.com>
2023-05-03 20:01:45 -0500

    Commit to prepare for push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,158 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Child = props =>{
+    const num1 = 23
+    const num2 = 42
+    const sum = num1 + num2
+    props.listen(`I CAN DO MATH! ${num1} + ${num2} is ${sum}`)
+    return <div> MATH IS FUN!</div>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Parent = () => {
+    const listenToTheChild = childWords =>{
+        console.log(`Passed back a variable!!: `, childWords)
+    }
+
+    return(
+        <>
+        <div>It's good to learn math! </div>
+        <Child listen={listenToTheChild} />
+        </>
+    )
+}
+
+ReactDOM.render(<Parent />, document.querySelector('#root') )
+
+//--------------------Example 1--------------------
+//
+// const Goodbye = () => {
+//     return <span>Goodbye cruel world!</span> 
+// }
+
+// function HelloWorld(){
+//     return (
+//         <>
+//             <div>HELLO WORLD (or literally anything else you want to write here)</div>
+//             <Goodbye />
+//         </>
+//     )
+// }
+//
+//
+//ReactDOM.render(<HelloWorld />, document.querySelector('#root'))
+//
+//
+//
+//
+//
+//  This below is used without react fragment and thus will require a div element to wrap the other elements 
+//
+//function HelloWorld(){
+//     return (
+//         <div>
+//             <div>HELLO WORLD (or literally anything else you want to write here)</div>
+//             <Goodbye />
+//         </div>
+//     )
+// }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//------------------Example 2--------------------
+//
+// const isFrench = true
+//
+// const Bonjour = () => {
+//     return <span>Bonjour</span>
+// }
+//
+// const Hello = () => {
+//     return <span>Hello</span>
+// }
+//
+// const sayHello = () =>{
+//     if(isFrench){
+//         return <Bonjour />
+//     }else {
+//         return <Hello />
+//     }
+// }
+//
+// const name = ' Mike'
+//
+// const FrenchHelloWorld = () => {
+//     return (
+//         <>
+//             <sayHello />
+//             {name}
+//         </>
+//     )
+// }
+//
+// ReactDOM.render(<FrenchHelloWorld />, document.querySelector('#root'))
+//
+//
+//
+//
+//
+//------------------- Example 3 ---------------------
+//
+//
+// const isLoggedIn = true
+// const username = 'reallyCoolGuy88'
+//
+// const LogInButton = () => (
+//     <button onClick={() => alert('Log in, bozo')}> DO THE LOG IN PLEASE</button>
+// )
+//
+// const DisplayLogin = () => {
+//     return <> {isLoggedIn && username} </>
+// }
+//
+// // const DisplayLogin = () => {
+// //     return <> {isLoggedIn ? username : <LogInButton />} </>
+// // }
+//
+//
+// ReactDOM.render(<DisplayLogin/>, document.querySelector('#root'))
+//
+//
+//
+//
+//
+//
+//
+//
+//------------------Example 4-------------------
+//
+//
+// const Hello = ({ name, address}) => {
+//     return (
+//         <>
+//         <span>Hello {name}</span>
+//         <span> I KNOW WHERE YOU LIVE {address}</span>
+//         </>
+//     )
+// }
+//
+// const SayHelloAndPassAProp = () => {
+//     return <Hello name={'Mike'} address={'123 Fake Street'} />
+// }
+//
+// // const Hello = props => {
+// //     return <span>Hello {props.name}</span>
+// // }
+//
+// // const SayHelloAndPassAProp = () => {
+// //     const somebodysName = 'Matt'
+// //     return <Hello name={somebodysName} />
+// // }
+//
+// ReactDOM.render(<SayHelloAndPassAProp />, document.querySelector('#root'))
